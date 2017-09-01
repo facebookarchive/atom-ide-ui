105989629918864688b35a3aac6c805a46f44ed5
Hanson Wang <hansonw@fb.com>
2017-09-01 15:53:21 -0700

    Move nuclide-jasmine to modules
    Summary:
    One more step towards having a self-contained modules directory.
    
    Note that I left behind a wrapper to avoid renaming all the `nuclide-jasmine` references. With Yarn workspaces this can actually be:
    
    But Yarn workspaces doesn't properly link `bin` from submodules yet so we'll just stay with this for now (T21523621)
    
    Reviewed By: wbinnssmith
    
    Differential Revision: D5751351
    
    fbshipit-source-id: 530211fd91b889d945a4a2ce14ad841cff1e503f

---
+++ nuclide-jasmine/bin/run-jasmine-tests.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @noflow
+ */
+'use strict';
+
+/* eslint
+  comma-dangle: [1, always-multiline],
+  prefer-object-spread/prefer-object-spread: 0,
+  rulesdir/no-commonjs: 0,
+  */
+/* eslint-disable no-console */
+
+// jasmine-node test runner with Atom test globals and babel transpiling support.
+
+// TODO(#21523621): Use a regular require once Yarn workspaces are enforced
+// eslint-disable-next-line rulesdir/modules-dependencies
+require('../../nuclide-node-transpiler');
+
+// Set this up before we call jasmine-node. jasmine-node does this same trick,
+// but neglects to respect the exit code, so we beat it the to the punch.
+process.once('exit', code => {
+  const temp = require('temp');
+  if (code === 0) {
+    // jasmine-node is swallowing temp's exit handler, so force a cleanup.
+    try {
+      temp.cleanupSync();
+    } catch (err) {
+      if (err && err.message !== 'not tracking') {
+        console.log(`temp.cleanup() failed. ${err}`);
+      }
+    }
+  } else {
+    // When the test fails, we keep the temp contents for debugging.
+    temp.track(false);
+  }
+  process.exit(code);
+});
+
+// Load waitsForPromise into global.
+global.waitsForPromise = require('../lib/waitsForPromise').default;
+global.window = global;
+
+require('../lib/focused');
+require('../lib/unspy');
+require('../lib/faketimer');
+
+try {
+  // This loads the CLI for jasmine-node.
+  require('jasmine-node/bin/jasmine-node');
+} catch (e) {
+  // Note that the process.exit(1) works now because of the "exit" handler
+  // installed at the start of this script.
+  console.error(e.toString());
+  console.error(e.stack);
+
+  process.exit(1);
+}
---
+++ nuclide-jasmine/lib/waitsForPromise.js
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import invariant from 'assert';
+
+type WaitsForPromiseOptions = {
+  shouldReject?: boolean,
+  timeout?: number,
+};
+
+export default function waitsForPromise(
+  ...args: Array<WaitsForPromiseOptions | (() => Promise<mixed>)>
+): void {
+  let shouldReject;
+  let timeout;
+  if (args.length > 1) {
+    shouldReject = args[0].shouldReject;
+    timeout = args[0].timeout;
+  } else {
+    shouldReject = false;
+    timeout = 0;
+  }
+
+  let finished = false;
+
+  runs(() => {
+    const fn = args[args.length - 1];
+    invariant(typeof fn === 'function');
+    const promise = fn();
+    if (shouldReject) {
+      promise
+        .then(
+          () => {
+            jasmine
+              .getEnv()
+              .currentSpec.fail(
+                'Expected promise to be rejected, but it was resolved',
+              );
+          },
+          () => {
+            // Do nothing, it's expected.
+          },
+        )
+        .then(() => {
+          finished = true;
+        });
+    } else {
+      promise
+        .then(
+          () => {
+            // Do nothing, it's expected.
+          },
+          error => {
+            const text = error ? error.stack || error.toString() : 'undefined';
+            jasmine
+              .getEnv()
+              .currentSpec.fail(
+                `Expected promise to be resolved, but it was rejected with ${text}`,
+              );
+          },
+        )
+        .then(() => {
+          finished = true;
+        });
+    }
+  });
+
+  waitsFor(timeout, () => finished);
+}
