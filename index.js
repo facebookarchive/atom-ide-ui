bfea622969776dd7830c526117f895230cb14242
Hanson Wang <hanson.wng@gmail.com>
2017-09-14 22:14:59 -0700

    Release v0.3.3

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+'use strict';
+
+var _fs = _interopRequireDefault(require('fs'));
+
+var _path = _interopRequireDefault(require('path'));
+
+var _UniversalDisposable;
+
+function _load_UniversalDisposable() {
+  return _UniversalDisposable = _interopRequireDefault(require('nuclide-commons/UniversalDisposable'));
+}
+
+var _FeatureLoader;
+
+function _load_FeatureLoader() {
+  return _FeatureLoader = _interopRequireDefault(require('nuclide-commons-atom/FeatureLoader'));
+}
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 /**
  * Copyright (c) 2017-present, Facebook, Inc.
  * All rights reserved.
@@ -6,103 +26,79 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- * @flow
+ * 
  * @format
  */
 
 /* eslint-disable rulesdir/no-commonjs */
 
-import fs from 'fs';
-// eslint-disable-next-line rulesdir/prefer-nuclide-uri
-import path from 'path';
-import UniversalDisposable from 'nuclide-commons/UniversalDisposable';
-import FeatureLoader from 'nuclide-commons-atom/FeatureLoader';
-
 const HIDE_WARNING_KEY = 'atom-ide-ui.hideNuclideWarning';
+// eslint-disable-next-line rulesdir/prefer-nuclide-uri
+
 
 function displayNuclideWarning() {
   if (!atom.config.get(HIDE_WARNING_KEY)) {
-    const notification = atom.notifications.addInfo(
-      'Atom IDE UI is bundled with Nuclide',
-      {
-        description:
-          '`atom-ide-ui` will be deactivated in favor of Nuclide.<br>' +
-          'Please disable Nuclide if you only want to use `atom-ide-ui`.',
-        dismissable: true,
-        buttons: [
-          {
-            text: 'Disable Nuclide and reload',
-            onDidClick() {
-              atom.packages.disablePackage('nuclide');
-              atom.reload();
-              notification.dismiss();
-            },
-          },
-          {
-            text: "Don't warn me again",
-            onDidClick() {
-              atom.config.set(HIDE_WARNING_KEY, true);
-              notification.dismiss();
-            },
-          },
-        ],
-      },
-    );
+    const notification = atom.notifications.addInfo('Atom IDE UI is bundled with Nuclide', {
+      description: '`atom-ide-ui` will be deactivated in favor of Nuclide.<br>' + 'Please disable Nuclide if you only want to use `atom-ide-ui`.',
+      dismissable: true,
+      buttons: [{
+        text: 'Disable Nuclide and reload',
+        onDidClick() {
+          atom.packages.disablePackage('nuclide');
+          atom.reload();
+          notification.dismiss();
+        }
+      }, {
+        text: "Don't warn me again",
+        onDidClick() {
+          atom.config.set(HIDE_WARNING_KEY, true);
+          notification.dismiss();
+        }
+      }]
+    });
   }
 }
 
-if (
-  !atom.packages.isPackageDisabled('nuclide') &&
-  atom.packages.getAvailablePackageNames().includes('nuclide')
-) {
+if (!atom.packages.isPackageDisabled('nuclide') && atom.packages.getAvailablePackageNames().includes('nuclide')) {
   displayNuclideWarning();
 } else {
-  const featureDir = path.join(__dirname, 'modules/atom-ide-ui/pkg');
-  const features = fs
-    .readdirSync(featureDir)
-    .map(item => {
-      const dirname = path.join(featureDir, item);
-      try {
-        const pkgJson = fs.readFileSync(
-          path.join(dirname, 'package.json'),
-          'utf8',
-        );
-        return {
-          dirname,
-          pkg: JSON.parse(pkgJson),
-        };
-      } catch (err) {
-        if (err.code !== 'ENOENT') {
-          throw err;
-        }
+  const featureDir = _path.default.join(__dirname, 'modules/atom-ide-ui/pkg');
+  const features = _fs.default.readdirSync(featureDir).map(item => {
+    const dirname = _path.default.join(featureDir, item);
+    try {
+      const pkgJson = _fs.default.readFileSync(_path.default.join(dirname, 'package.json'), 'utf8');
+      return {
+        dirname,
+        pkg: JSON.parse(pkgJson)
+      };
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
       }
-    })
-    .filter(Boolean);
+    }
+  }).filter(Boolean);
 
   /**
    * Use a unified package loader to load all the feature packages.
    * See the following post for more context:
    * https://nuclide.io/blog/2016/01/13/Nuclide-v0.111.0-The-Unified-Package/
    */
-  let disposables: ?UniversalDisposable;
-  const featureLoader = new FeatureLoader({
+  let disposables;
+  const featureLoader = new (_FeatureLoader || _load_FeatureLoader()).default({
     pkgName: 'atom-ide-ui',
     config: {},
-    features,
+    features
   });
   featureLoader.load();
 
   module.exports = {
     config: featureLoader.getConfig(),
     activate() {
-      disposables = new UniversalDisposable(
-        require('nuclide-commons-ui'),
-        atom.packages.onDidActivatePackage(pkg => {
-          if (pkg.name === 'nuclide') {
-            displayNuclideWarning();
-          }
-        }),
-      );
+      disposables = new (_UniversalDisposable || _load_UniversalDisposable()).default(require('nuclide-commons-ui'), atom.packages.onDidActivatePackage(pkg => {
+        if (pkg.name === 'nuclide') {
+          displayNuclideWarning();
+        }
+      }));
       featureLoader.activate();
     },
     deactivate() {
@@ -114,6 +110,6 @@ if (
     },
     serialize() {
       featureLoader.serialize();
-    },
+    }
   };
 }
