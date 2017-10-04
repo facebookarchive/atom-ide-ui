1a1503f77680a306ddde941988afff6555f7d916
Will Binns-Smith <wbinnssmith@fb.com>
2017-10-04 15:54:20 -0700

    Have showModal activate a focus trap when shown
    Summary:
    This locks tabbing to within the modal, and attempts to focus the first
    tabbable element
    
    Reviewed By: matthewwithanm
    
    Differential Revision: D5970769
    
    fbshipit-source-id: 396dd85f5600360fbba012aaebfeeadffa42ab0d

---
+++ modules/nuclide-commons-ui/RadioGroup.example.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import * as React from 'react';
+import {Block} from './Block';
+import RadioGroup from './RadioGroup';
+
+const labels = ['choose', 'from', 'one of', 'several', 'options'];
+
+class RadioGroupExample extends React.Component<any, {selectedIndex: number}> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      selectedIndex: 0,
+    };
+  }
+
+  onSelectedChange = (selectedIndex: number): void => {
+    this.setState({
+      selectedIndex,
+    });
+  };
+
+  render(): React.Node {
+    return (
+      <Block>
+        {/* $FlowFixMe(>=0.53.0) Flow suppress */}
+        <RadioGroup
+          selectedIndex={this.state.selectedIndex}
+          optionLabels={labels}
+          onSelectedChange={this.onSelectedChange}
+        />
+      </Block>
+    );
+  }
+}
+
+export const RadioGroupExamples = {
+  sectionName: 'RadioGroup',
+  description: '',
+  examples: [
+    {
+      title: '',
+      component: RadioGroupExample,
+    },
+  ],
+};
---
+++ modules/nuclide-commons-ui/RadioGroup.js
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import * as React from 'react';
+
+// Globally unique ID used as the "name" attribute to group radio inputs.
+let uid = 0;
+
+type Props = {
+  className?: string,
+  optionLabels: Array<React.Element<any>>,
+  selectedIndex: number,
+  onSelectedChange(selectedIndex: number): void,
+};
+
+type State = {
+  uid: number,
+};
+
+/**
+ * A managed radio group component. Accepts arbitrary React elements as labels.
+ */
+export default class RadioGroup extends React.Component<Props, State> {
+  static defaultProps = {
+    optionLabels: [],
+    onSelectedChange: (selectedIndex: number) => {},
+    selectedIndex: 0,
+  };
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      uid: uid++,
+    };
+  }
+
+  render(): React.Node {
+    const {
+      className,
+      onSelectedChange,
+      optionLabels,
+      selectedIndex,
+    } = this.props;
+    const checkboxes = optionLabels.map((labelContent, i) => {
+      const id = 'nuclide-radiogroup-' + uid + '-' + i;
+      return (
+        <div key={i} className="nuclide-ui-radiogroup-div">
+          <input
+            className="input-radio"
+            type="radio"
+            checked={i === selectedIndex}
+            name={'radiogroup-' + this.state.uid}
+            id={id}
+            onChange={() => {
+              onSelectedChange(i);
+            }}
+          />
+          <label
+            className="input-label nuclide-ui-radiogroup-label"
+            htmlFor={id}>
+            {labelContent}
+          </label>
+        </div>
+      );
+    });
+    return <div className={className}>{checkboxes}</div>;
+  }
+}
