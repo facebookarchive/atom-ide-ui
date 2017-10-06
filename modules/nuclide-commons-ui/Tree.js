34cf7d38c0daaa2210bbb841df2cd55c0a772409
Will Binns-Smith <wbinnssmith@fb.com>
2017-10-06 15:50:53 -0700

    Move Tree module to nuclide-commons-ui
    Summary:
    Moves the Tree module and associated examples to nuclide-commons-ui.
    
    Updates corresponding `import`s.
    
    I'd like to use it in outline view, which is part of Atom IDE and needs this.
    
    Reviewed By: jgebhardt
    
    Differential Revision: D5983924
    
    fbshipit-source-id: 5b274df4548ed311710e51ec971c7c5dfa71bcd7

---
+++ modules/nuclide-commons-ui/Tree.js
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ * @format
+ */
+
+import * as React from 'react';
+import classnames from 'classnames';
+import {scrollIntoView} from './scrollIntoView';
+
+type TreeItemProps = {
+  children?: mixed,
+  className?: string,
+  selected?: boolean,
+};
+
+export class TreeItem extends React.Component<TreeItemProps> {
+  _liNode: ?Element;
+
+  scrollIntoView() {
+    if (this._liNode != null) {
+      scrollIntoView(this._liNode);
+    }
+  }
+
+  render() {
+    const {className, selected, children, ...remainingProps} = this.props;
+    return (
+      // $FlowFixMe(>=0.53.0) Flow suppress
+      <li
+        className={classnames(
+          className,
+          {
+            selected,
+          },
+          'list-item',
+        )}
+        {...remainingProps}
+        ref={liNode => (this._liNode = liNode)}>
+        {selected && typeof children === 'string' ? (
+          // String children must be wrapped to receive correct styles when selected.
+          <span>{children}</span>
+        ) : (
+          children
+        )}
+      </li>
+    );
+  }
+}
+
+type NestedTreeItemProps = {
+  title?: ?React.Element<any>,
+  children?: mixed,
+  className?: string,
+  hasFlatChildren?: boolean, // passthrough to inner TreeList
+  selected?: boolean,
+  collapsed?: boolean,
+  onClick?: (event: SyntheticMouseEvent<>) => void,
+  onDoubleClick?: (event: SyntheticMouseEvent<>) => void,
+};
+export const NestedTreeItem = (props: NestedTreeItemProps) => {
+  const {
+    className,
+    hasFlatChildren,
+    selected,
+    collapsed,
+    title,
+    children,
+    onClick,
+    onDoubleClick,
+    ...remainingProps
+  } = props;
+  return (
+    <li
+      className={classnames(
+        className,
+        {
+          selected,
+          collapsed,
+        },
+        'list-nested-item',
+      )}
+      {...remainingProps}>
+      {title ? (
+        <div
+          className="list-item"
+          onClick={onClick}
+          onDoubleClick={onDoubleClick}>
+          {title}
+        </div>
+      ) : null}
+      <TreeList hasFlatChildren={hasFlatChildren}>{children}</TreeList>
+    </li>
+  );
+};
+
+type TreeListProps = {
+  className?: string,
+  /* typically, instances of TreeItem or NestedTreeItem. */
+  children?: mixed,
+  showArrows?: boolean,
+  hasFlatChildren?: boolean,
+};
+export const TreeList = (props: TreeListProps) => (
+  // $FlowFixMe(>=0.53.0) Flow suppress
+  <ul
+    className={classnames(
+      props.className,
+      {
+        'has-collapsable-children': props.showArrows,
+        'has-flat-children': props.hasFlatChildren,
+      },
+      'list-tree',
+    )}>
+    {props.children}
+  </ul>
+);
