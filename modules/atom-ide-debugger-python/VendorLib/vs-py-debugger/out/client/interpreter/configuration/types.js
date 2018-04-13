ebd9b773596a504ded0b2687e3af7ef40866028f
Mostafa Eweda <most@fb.com>
2018-04-13 09:43:30 -0700

    Remove already synced atom-ide-debugger-*

--- modules/atom-ide-debugger-python/VendorLib/vs-py-debugger/out/client/interpreter/configuration/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IPythonPathUpdaterServiceFactory = Symbol('IPythonPathUpdaterServiceFactory');
-exports.IPythonPathUpdaterServiceManager = Symbol('IPythonPathUpdaterServiceManager');
-exports.IInterpreterSelector = Symbol('IInterpreterSelector');
-//# sourceMappingURL=types.js.map
--- modules/atom-ide-debugger-python/VendorLib/vs-py-debugger/pythonFiles/sortImports.py
+++
@@ -1,2 +0,0 @@
-import isort.main
-isort.main.main()
