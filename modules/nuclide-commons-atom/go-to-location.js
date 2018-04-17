f90e165a20ff5e46d867fddf859ae6bfdf61858a
Mostafa Eweda <most@fb.com>
2018-04-16 21:05:01 -0700

    Release v0.10.1

--- modules/nuclide-commons-atom/go-to-location.js
+++ modules/nuclide-commons-atom/go-to-location.js
@@ -1,31 +1,11 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- * @flow
- * @format
- */
+'use strict';
 
-import type {Observable} from 'rxjs';
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.goToLocation = undefined;
 
-import {getLogger} from 'log4js';
-import {Subject} from 'rxjs';
-import invariant from 'assert';
-import idx from 'idx';
-
-export type GoToLocationOptions = {|
-  line?: number,
-  column?: number,
-  center?: boolean,
-  activateItem?: boolean,
-  activatePane?: boolean,
-  pending?: boolean,
-  moveCursor?: boolean,
-|};
+var _asyncToGenerator = _interopRequireDefault(require('async-to-generator'));
 
 /**
  * Opens the given file.
@@ -51,97 +31,129 @@ export type GoToLocationOptions = {|
  * following comment above its use:
  * // eslint-disable-next-line rulesdir/atom-apis
  */
-export async function goToLocation(
-  file: string,
-  options?: ?GoToLocationOptions,
-): Promise<atom$TextEditor> {
-  const center_ = idx(options, _ => _.center);
-  const center = center_ == null ? true : center_;
-  const moveCursor_ = idx(options, _ => _.moveCursor);
-  const moveCursor = moveCursor_ == null ? true : moveCursor_;
-  const activatePane_ = idx(options, _ => _.activatePane);
-  const activatePane = activatePane_ == null ? true : activatePane_;
-  const activateItem = idx(options, _ => _.activateItem);
-  const line = idx(options, _ => _.line);
-  const column = idx(options, _ => _.column);
-  const pending = idx(options, _ => _.pending);
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * 
+ * @format
+ */
 
-  // Prefer going to the current editor rather than the leftmost editor.
-  const currentEditor = atom.workspace.getActiveTextEditor();
-  if (currentEditor != null && currentEditor.getPath() === file) {
-    const paneContainer = atom.workspace.paneContainerForItem(currentEditor);
-    invariant(paneContainer != null);
-    if (activatePane) {
-      paneContainer.activate();
-    }
-    if (line != null) {
-      goToLocationInEditor(currentEditor, {
-        line,
-        column: column == null ? 0 : column,
-        center,
-        moveCursor,
-      });
+let goToLocation = exports.goToLocation = (() => {
+  var _ref8 = (0, _asyncToGenerator.default)(function* (file, options) {
+    var _ref, _ref2, _ref3, _ref4, _ref5, _ref6, _ref7;
+
+    const center_ = (_ref = options) != null ? _ref.center : _ref;
+    const center = center_ == null ? true : center_;
+    const moveCursor_ = (_ref2 = options) != null ? _ref2.moveCursor : _ref2;
+    const moveCursor = moveCursor_ == null ? true : moveCursor_;
+    const activatePane_ = (_ref3 = options) != null ? _ref3.activatePane : _ref3;
+    const activatePane = activatePane_ == null ? true : activatePane_;
+    const activateItem = (_ref4 = options) != null ? _ref4.activateItem : _ref4;
+    const line = (_ref5 = options) != null ? _ref5.line : _ref5;
+    const column = (_ref6 = options) != null ? _ref6.column : _ref6;
+    const pending = (_ref7 = options) != null ? _ref7.pending : _ref7;
+
+    // Prefer going to the current editor rather than the leftmost editor.
+    const currentEditor = atom.workspace.getActiveTextEditor();
+    if (currentEditor != null && currentEditor.getPath() === file) {
+      const paneContainer = atom.workspace.paneContainerForItem(currentEditor);
+
+      if (!(paneContainer != null)) {
+        throw new Error('Invariant violation: "paneContainer != null"');
+      }
+
+      if (activatePane) {
+        paneContainer.activate();
+      }
+      if (line != null) {
+        goToLocationInEditor(currentEditor, {
+          line,
+          column: column == null ? 0 : column,
+          center,
+          moveCursor
+        });
+      } else {
+        if (!(column == null)) {
+          throw new Error('goToLocation: Cannot specify just column');
+        }
+      }
+      return currentEditor;
     } else {
-      invariant(column == null, 'goToLocation: Cannot specify just column');
-    }
-    return currentEditor;
-  } else {
-    // Obviously, calling goToLocation isn't a viable alternative here :P
-    // eslint-disable-next-line rulesdir/atom-apis
-    const editor = await atom.workspace.open(file, {
-      initialLine: line,
-      initialColumn: column,
-      searchAllPanes: true,
-      activatePane,
-      activateItem,
-      pending,
-    });
-    // TODO(T28305560) Investigate offenders for this error
-    if (editor == null) {
-      const tmp = {};
-      Error.captureStackTrace(tmp);
-      const error = Error(`atom.workspace.open returned null on ${file}`);
-      getLogger('goToLocation').error(error);
-      throw error;
-    }
+      // Obviously, calling goToLocation isn't a viable alternative here :P
+      // eslint-disable-next-line rulesdir/atom-apis
+      const editor = yield atom.workspace.open(file, {
+        initialLine: line,
+        initialColumn: column,
+        searchAllPanes: true,
+        activatePane,
+        activateItem,
+        pending
+      });
+      // TODO(T28305560) Investigate offenders for this error
+      if (editor == null) {
+        const tmp = {};
+        Error.captureStackTrace(tmp);
+        const error = Error(`atom.workspace.open returned null on ${file}`);
+        (0, (_log4js || _load_log4js()).getLogger)('goToLocation').error(error);
+        throw error;
+      }
 
-    if (center && line != null) {
-      editor.scrollToBufferPosition([line, column], {center: true});
+      if (center && line != null) {
+        editor.scrollToBufferPosition([line, column], { center: true });
+      }
+      return editor;
     }
-    return editor;
-  }
+  });
+
+  return function goToLocation(_x, _x2) {
+    return _ref8.apply(this, arguments);
+  };
+})();
+
+exports.goToLocationInEditor = goToLocationInEditor;
+exports.observeNavigatingEditors = observeNavigatingEditors;
+
+var _log4js;
+
+function _load_log4js() {
+  return _log4js = require('log4js');
 }
 
-const goToLocationSubject = new Subject();
+var _rxjsBundlesRxMinJs = require('rxjs/bundles/Rx.min.js');
 
-type GotoLocationInEditorOptions = {|
-  line: number,
-  column: number,
-  center?: boolean,
-  moveCursor?: boolean,
-|};
+var _idx;
+
+function _load_idx() {
+  return _idx = _interopRequireDefault(require('idx'));
+}
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const goToLocationSubject = new _rxjsBundlesRxMinJs.Subject();
 
 // Scrolls to the given line/column at the given editor
 // broadcasts the editor instance on an observable (subject) available
 // through the getGoToLocation
-export function goToLocationInEditor(
-  editor: atom$TextEditor,
-  options: GotoLocationInEditorOptions,
-): void {
+function goToLocationInEditor(editor, options) {
   const center = options.center == null ? true : options.center;
   const moveCursor = options.moveCursor == null ? true : options.moveCursor;
-  const {line, column} = options;
+  const { line, column } = options;
 
   if (moveCursor) {
     editor.setCursorBufferPosition([line, column]);
   }
   if (center) {
-    editor.scrollToBufferPosition([line, column], {center: true});
+    editor.scrollToBufferPosition([line, column], { center: true });
   }
 
   goToLocationSubject.next(editor);
 }
 
-export function observeNavigatingEditors(): Observable<atom$TextEditor> {
+function observeNavigatingEditors() {
   return goToLocationSubject;
 }
